1bc5bc38470301e5cd9caca039f0f0ab0ad7c67f
Alfredo Kylle Navarro <83118328+AlfredoKylle82@users.noreply.github.com>
2023-07-14 00:11:01 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace G10GP02_NAVARRO_HT2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.chkUniform = new System.Windows.Forms.CheckBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.chkTextbook = new System.Windows.Forms.CheckBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblGradeLevel = new System.Windows.Forms.Label();
+            this.lblItems = new System.Windows.Forms.Label();
+            this.lblPayments = new System.Windows.Forms.Label();
+            this.rdoMonths2 = new System.Windows.Forms.RadioButton();
+            this.tbxResult = new System.Windows.Forms.TextBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxGradelevel = new System.Windows.Forms.TextBox();
+            this.rdoMonths5 = new System.Windows.Forms.RadioButton();
+            this.lblSchedule = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(315, 206);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(138, 34);
+            this.btnCompute.TabIndex = 0;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chkUniform
+            // 
+            this.chkUniform.AutoSize = true;
+            this.chkUniform.Location = new System.Drawing.Point(56, 101);
+            this.chkUniform.Name = "chkUniform";
+            this.chkUniform.Size = new System.Drawing.Size(62, 17);
+            this.chkUniform.TabIndex = 1;
+            this.chkUniform.Text = "Uniform";
+            this.chkUniform.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(315, 246);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(138, 34);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(315, 286);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(138, 32);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chkTextbook
+            // 
+            this.chkTextbook.AutoSize = true;
+            this.chkTextbook.Location = new System.Drawing.Point(183, 101);
+            this.chkTextbook.Name = "chkTextbook";
+            this.chkTextbook.Size = new System.Drawing.Size(72, 17);
+            this.chkTextbook.TabIndex = 4;
+            this.chkTextbook.Text = "TextBook";
+            this.chkTextbook.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(53, 42);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name :";
+            // 
+            // lblGradeLevel
+            // 
+            this.lblGradeLevel.AutoSize = true;
+            this.lblGradeLevel.Location = new System.Drawing.Point(276, 38);
+            this.lblGradeLevel.Name = "lblGradeLevel";
+            this.lblGradeLevel.Size = new System.Drawing.Size(71, 13);
+            this.lblGradeLevel.TabIndex = 6;
+            this.lblGradeLevel.Text = "Grade Level :";
+            // 
+            // lblItems
+            // 
+            this.lblItems.AutoSize = true;
+            this.lblItems.Location = new System.Drawing.Point(53, 75);
+            this.lblItems.Name = "lblItems";
+            this.lblItems.Size = new System.Drawing.Size(167, 13);
+            this.lblItems.TabIndex = 7;
+            this.lblItems.Text = "Please Check Items to Purchase :";
+            // 
+            // lblPayments
+            // 
+            this.lblPayments.AutoSize = true;
+            this.lblPayments.Location = new System.Drawing.Point(53, 146);
+            this.lblPayments.Name = "lblPayments";
+            this.lblPayments.Size = new System.Drawing.Size(105, 13);
+            this.lblPayments.TabIndex = 8;
+            this.lblPayments.Text = "Number of Payments";
+            // 
+            // rdoMonths2
+            // 
+            this.rdoMonths2.AutoSize = true;
+            this.rdoMonths2.Location = new System.Drawing.Point(56, 171);
+            this.rdoMonths2.Name = "rdoMonths2";
+            this.rdoMonths2.Size = new System.Drawing.Size(69, 17);
+            this.rdoMonths2.TabIndex = 10;
+            this.rdoMonths2.TabStop = true;
+            this.rdoMonths2.Text = "2 Months";
+            this.rdoMonths2.UseVisualStyleBackColor = true;
+            // 
+            // tbxResult
+            // 
+            this.tbxResult.Location = new System.Drawing.Point(74, 206);
+            this.tbxResult.Multiline = true;
+            this.tbxResult.Name = "tbxResult";
+            this.tbxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxResult.Size = new System.Drawing.Size(235, 122);
+            this.tbxResult.TabIndex = 11;
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(100, 39);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(100, 20);
+            this.tbxName.TabIndex = 12;
+            // 
+            // tbxGradelevel
+            // 
+            this.tbxGradelevel.Location = new System.Drawing.Point(353, 35);
+            this.tbxGradelevel.Name = "tbxGradelevel";
+            this.tbxGradelevel.Size = new System.Drawing.Size(100, 20);
+            this.tbxGradelevel.TabIndex = 13;
+            // 
+            // rdoMonths5
+            // 
+            this.rdoMonths5.AutoSize = true;
+            this.rdoMonths5.Location = new System.Drawing.Point(183, 171);
+            this.rdoMonths5.Name = "rdoMonths5";
+            this.rdoMonths5.Size = new System.Drawing.Size(69, 17);
+            this.rdoMonths5.TabIndex = 16;
+            this.rdoMonths5.TabStop = true;
+            this.rdoMonths5.Text = "5 Months";
+            this.rdoMonths5.UseVisualStyleBackColor = true;
+            // 
+            // lblSchedule
+            // 
+            this.lblSchedule.AutoSize = true;
+            this.lblSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSchedule.Location = new System.Drawing.Point(154, 9);
+            this.lblSchedule.Name = "lblSchedule";
+            this.lblSchedule.Size = new System.Drawing.Size(217, 20);
+            this.lblSchedule.TabIndex = 17;
+            this.lblSchedule.Text = "Tuition Payment Schedule";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 340);
+            this.Controls.Add(this.lblSchedule);
+            this.Controls.Add(this.rdoMonths5);
+            this.Controls.Add(this.tbxGradelevel);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.tbxResult);
+            this.Controls.Add(this.rdoMonths2);
+            this.Controls.Add(this.lblPayments);
+            this.Controls.Add(this.lblItems);
+            this.Controls.Add(this.lblGradeLevel);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.chkTextbook);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.chkUniform);
+            this.Controls.Add(this.btnCompute);
+            this.Name = "Form1";
+            this.Text = "G10GP02_HT2_NAVARRO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.CheckBox chkUniform;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox chkTextbook;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblGradeLevel;
+        private System.Windows.Forms.Label lblPayments;
+        private System.Windows.Forms.RadioButton rdoMonths2;
+        private System.Windows.Forms.TextBox tbxResult;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxGradelevel;
+        private System.Windows.Forms.RadioButton rdoMonths5;
+        private System.Windows.Forms.Label lblSchedule;
+        public System.Windows.Forms.Label lblItems;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G10GP02_NAVARRO_HT2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sName = "";
+            sName = tbxName.Text;
+
+            double dGrade = 0;
+            
+
+            double dMisc = 0;
+            double dBooks = 0;
+            double dUniform = 0;
+            string sMisc = "";
+            double dTotalmisc = 0;
+            double dAnnual = 0;
+            double dMonthly = 0;
+            double dRmonthly = 0;
+            int iMonthly = 0;
+            int iLoops = 0;
+            int iRt = 0;
+            int iT = 0;
+            string sRt = "";
+
+
+
+
+            iMonthly = 1;
+            dUniform = 2800.00;
+            dBooks = 3120.00;
+            dAnnual = 85000.00;
+
+
+
+
+            if (chkTextbook.Checked)
+            {
+                sMisc = sMisc + "Textbook";
+                dTotalmisc = dTotalmisc + dBooks;
+            }
+            if (chkUniform.Checked)
+            {
+                sMisc = sMisc + " Uniform";
+                dTotalmisc = dTotalmisc + dBooks;
+            }
+            if (rdoMonths2.Checked)
+            {
+                dMonthly = dMonthly + 85000 / 2;
+                iLoops = 2;
+                dRmonthly = dMonthly;
+            }
+            if (rdoMonths5.Checked)
+            {
+                dMonthly = dMonthly + 85000 / 5;
+                iLoops = 5;
+                dRmonthly = dMonthly;
+
+            }
+            while (iLoops > 0)
+            {
+                sRt = sRt + "\r\nMonth " + iMonthly + " - Php " + dRmonthly;
+                dRmonthly = dRmonthly + dMonthly;
+                iMonthly = iMonthly + 1;
+                iLoops = iLoops - 1;
+
+            }
+
+
+            tbxResult.Text = "Name: " + tbxName.Text + "\r\nGrade Level: "
+                                + tbxGradelevel.Text + "\r\nMiscellaneous Items: "
+                                + sMisc + "\r\nTotal Miscellaneous: " + dTotalmisc +
+                                "\r\n\r\nTotal Annual Tuition Fee Due: Php" + dAnnual + "\r\nMonthly Due: Php" +
+                                dMonthly + "\r\nRunning Total:" + sRt;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tbxName.Text = "";
+            tbxGradelevel.Text = "";
+            tbxResult.Text = "";
+            rdoMonths2.Checked = false;
+            rdoMonths5.Checked = false;
+            chkTextbook.Checked = false;
+            chkUniform.Checked = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
